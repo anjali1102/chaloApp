b71a15ab848b940f36bf02ecda57df691e61b1f2
Ankush Chauhan <ac.chauhan1102@gmail.com>
2022-11-07 22:38:15 +0530

    [Feat]: Map display and basic UI

--- src/App.js
+++ src/App.js
@@ -1,28 +1,66 @@
-import { useEffect, useRef, useState } from "react";
-import * as tt from "@tomtom-international/web-sdk-maps";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Flex,
+  HStack,
+  IconButton,
+  Input,
+  Text,
+} from "@chakra-ui/react";
+import { FaLocationArrow, FaTimes } from "react-icons/fa";
+import { Map } from "./components/map/Map";
+import { RoutingMachine } from "./components/RoutingMachine/RoutingMachine";
+
 import "./App.css";
 
 function App() {
-  const mapElement = useRef();
-  const [map, setMap] = useState({}); // to set the map
-
-  const longitude = -0.112869;
-  const latitude = 51.504;
-
-  //from docs for getting the map
-  useEffect(() => {
-    let map = tt.map({
-      key: process.env.REACT_APP_TOM_TOM_API_KEY,
-      container: mapElement.current,
-      center: [longitude, latitude],
-      zoom: 14,
-    });
-
-    setMap(map);
-  }, []);
   return (
     <div className="App">
-      <div ref={mapElement} className="map"></div>
+      {/* <RoutingMachine /> */}
+      <Flex
+        position="relative"
+        flexDirection="column"
+        alignItems="center"
+        h="100vh"
+        w="100vw"
+      >
+        <Box position="absolute" left={0} top={0} h="100%" w="100%">
+          <Map />
+        </Box>
+        <Box
+          p={4}
+          borderRadius="lg"
+          m={4}
+          bgColor="white"
+          shadow="base"
+          minW="container.md"
+          zIndex="999"
+        >
+          <HStack spacing={2} justifyContent="space-between">
+            <Box flexGrow={1}>
+              <Input type="text" placeholder="Origin" />
+            </Box>
+            <Box flexGrow={1}>
+              <Input type="text" placeholder="Destination" />
+            </Box>
+
+            <ButtonGroup>
+              <Button colorScheme="green">Calculate Route</Button>
+              <IconButton aria-label="center back" icon={<FaTimes />} />
+            </ButtonGroup>
+          </HStack>
+          <HStack spacing={4} mt={4} justifyContent="space-between">
+            <Text>Distance: </Text>
+            <Text>Duration: </Text>
+            <IconButton
+              aria-label="center back"
+              icon={<FaLocationArrow />}
+              isRound
+            />
+          </HStack>
+        </Box>
+      </Flex>
     </div>
   );
 }
---
+++ src/components/map/Map.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Marker, Popup, MapContainer, TileLayer, useMap } from "react-leaflet";
+import L from "leaflet";
+import "./Map.css";
+
+const Map = () => {
+  const position = [51.505, -0.09];
+  const customMarker = new L.icon({
+    iconUrl: "https://unpkg.com/leaflet@1.5.1/dist/images/marker-icon.png",
+    iconSize: [25, 41],
+    iconAnchor: [10, 41],
+    popupAnchor: [2, -40],
+  });
+
+  return (
+    <div>
+      <MapContainer
+        center={position}
+        zoom={6}
+        scrollWheelZoom={false}
+        style={{ width: "100%", height: "100vh" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position} icon={customMarker}>
+          {/* <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup> */}
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+};
+
+export { Map };
